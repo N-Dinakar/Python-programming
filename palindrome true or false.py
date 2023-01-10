d23dce65ddcfed321d1167d7cf821c6a9e6b2c78
N-Dinakar <114988477+N-Dinakar@users.noreply.github.com>
2023-01-10 11:17:48 +0530

    Add files via upload

---
+++ palindrome true or false.py
@@ -0,0 +1,8 @@
+string=input("Enter the string:")
+if string==string[ : :-1]:
+    print("True")
+    print("The given string is a palindrom")
+    
+else:
+    print("False")
+    print("The given string is not a palindrom")
